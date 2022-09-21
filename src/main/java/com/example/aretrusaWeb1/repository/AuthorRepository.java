745d86a8a60f79eeb9d6f24057b5d15d70b08cf4
fedepe00 <federico9b@libero.it>
2022-09-21 10:03:00 +0200

    GenreService

--- src/main/java/com/example/aretrusaWeb1/repository/AuthorRepository.java
+++ src/main/java/com/example/aretrusaWeb1/repository/AuthorRepository.java
@@ -6,5 +6,4 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AuthorRepository extends MongoRepository<Author, ObjectId> {
-
 }
