c2d5edd10492123d64df59ec356fadeb477e5e49
pierodelorenzis <delorenzispiero@gmail.com>
2022-09-23 11:08:03 +0200

    Update

--- src/main/java/com/example/aretrusaWeb1/facade/AuthorFacade.java
+++ src/main/java/com/example/aretrusaWeb1/facade/AuthorFacade.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class AAuthorFacade {
+public class AuthorFacade {
 
     @Autowired
     private AuthorService authorService;
@@ -61,4 +61,3 @@ public class AAuthorFacade {
 
 
 }
-
